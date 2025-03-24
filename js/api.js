112ff11712db4c80c2e8456297128b062dc03648
Tymo <tymo.smids@gmail.com>
2025-03-24 12:20:21 +0100

    added css grid

--- js/api.js
+++ js/api.js
@@ -6,6 +6,51 @@ const MyURL = baseURL + endpoint;
 
 getData(MyURL).then(dataWomen => {
     console.log(dataWomen);
+
+    let allWoman = dataWomen.data;
+
+    // Get container in the dom 
+    let everybodySection = document.querySelector(".womanContainer");
+
+    allWoman.forEach(woman => {
+
+        // Person variables
+        womanName = woman.name;
+        womanWork = woman.work;
+        womanTagline = woman.tagline;
+        womanPeriod = woman.period;
+        womanCountry = woman.country;
+        womanImage = 'https://fdnd.directus.app/assets/' + woman.image;
+        womanWebsite = woman.website;
+
+        /*
+        womanInfo = [womanName, womanWork, womanTagline, womanPeriod, womanCountry, womanImage, womanWebsite];
+
+        for (let info = 0; info < womanInfo.length; info++) {
+            const infoElement = womanInfo[info];
+
+            if(infoElement == null) {
+                infoElement = 'Geen waarde gegeven';
+            }
+            
+        }
+            */
+
+        let womanHTML =  
+        `<article>
+            <img src="${womanImage}" loading="lazy" alt="${womanName}" />
+            <div class="info">
+                <h3 class="name">${womanName}</h3>
+                <small class="quote">${womanTagline}</small>
+                <p class="work"><i class="fa-solid fa-briefcase"></i> ${womanWork}</p>
+                <p class="country"><i class="fa-solid fa-earth-europe"></i> ${womanCountry}</p>
+                <p class="period">${womanPeriod}</p>
+                <a href="${womanWebsite}" aria-label="de website van ${womanName}">More information <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+            </div>
+        </article>`;
+
+        everybodySection.insertAdjacentHTML('beforeend', womanHTML);
+    });
 });
 
 // Get Json data 
